69018594127f0713f53ce4a940c1706e753f15b2
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-07 17:33:49 +0600

    admin route

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaHamburger, FaShoppingBag, FaPhone, FaUtensils, FaBook, FaUsers } from "react-icons/fa";
 import useCart from "../Hooks/useCart";
+import useAdmin from "../Hooks/useAdmin";
 const Dashboard = () => {
     const [cart] = useCart();
 
     // TODO: load data from the server to have dynamic isAdmin based on Data
-    const isAdmin = true;
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
 
     return (
         <div className="drawer drawer-mobile">
